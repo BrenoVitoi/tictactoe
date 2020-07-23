98886d13ee205ebe14052fc46da03225afc4de93
Breno Souza <breno.vitoi@gmail.com>
2020-07-23 10:00:29 -0300

    Primeiro commit-carregando jogo parcial

---
+++ tictactoe/src/main/java/br/com/softblue/tictactoe/Main.java
@@ -0,0 +1,14 @@
+package br.com.softblue.tictactoe;
+
+import br.com.softblue.tictactoe.core.Game;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Game g = new Game();
+		g.play(); 
+
+	}
+
+}
---
+++ tictactoe/src/main/java/br/com/softblue/tictactoe/core/Move.java
@@ -0,0 +1,28 @@
+package br.com.softblue.tictactoe.core;
+
+public class Move {
+	
+	private int i;
+	private int j;
+	
+	public Move(String moveStr) {
+		String[] tokens = moveStr.split(",");
+		
+		this.i = Integer.parseInt(tokens[0]) ;
+		this.j = Integer.parseInt(tokens[1]);
+		
+		//TODO Validar se a estrutura do moveStr está certa
+	}
+	
+	public int getI() {
+		return i;
+	}
+	public int getJ() {
+		return j;
+	}
+	
+	
+	
+	
+
+}
